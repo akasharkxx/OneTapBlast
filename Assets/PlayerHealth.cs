a67288e579618b4d497a1cd93f0bf7f8257bfc72
akasharkxx <akash.jaiswar41@gmail.com>
2020-09-07 00:16:01 +0530

    Added PlayerHealth.cs and AsteroidDamage.cs
    Added an apk for v0.01

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private int maxHealth;
+
+    private int currentHealth;
+    private bool isDead;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;    
+    }
+
+    private void Update()
+    {
+        healthText.text = currentHealth.ToString();   
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            isDead = true;
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public bool GetisDead()
+    {
+        return isDead;
+    }
+}
